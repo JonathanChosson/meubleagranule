ed1b12bf2fdb0dcd3003b75aa4b306e30be9e1ff
Jonathan Chosson <jonathan.chosson@gmail.com>
2022-10-18 10:15:14 +0200

    [JSX/Style] Premier rendu Header :lipstick:

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from "react"
+import Header from "../components/Header"
 import "../Style/Pages/Home.css"
 
 const Home = () => {
-    return <div className="Home"></div>
+    return (
+        <div className="Home">
+            <Header />
+            <article className="Home__article">
+                <img
+                    className="Home__article__img"
+                    src="./image/fond.jpeg"
+                    alt="Chauffage granule"
+                />
+            </article>
+        </div>
+    )
 }
 
 export default Home
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,38 @@ import React from "react"
 import "../Style/Components/Header.css"
 
 const Header = () => {
-    return <div className="Header"></div>
+    function toggleNav() {
+        console.log("click")
+        const navItem = document.querySelector(".Header__nav__ul")
+        navItem.classList.toggle("visible")
+    }
+    return (
+        <div className="Header">
+            <h1 className="Header__h1">
+                <span className="Rouge">Meuble</span>a
+                <span className="Rouge">Granule</span>.fr
+            </h1>
+            <nav className="Header__nav">
+                <i
+                    className="fa-solid fa-bars Header__nav__hamburger"
+                    onClick={toggleNav}
+                ></i>
+                <ul className="Header__nav__ul">
+                    <li className="Header__nav__ul__li">
+                        <i className="fa-solid fa-table-columns"></i> Tableau de
+                        bord
+                    </li>
+                    <li className="Header__nav__ul__li">
+                        <i className="fa-solid fa-user"></i> Profil
+                    </li>
+                    <li className="Header__nav__ul__li">
+                        <i className="fa-solid fa-right-from-bracket"></i>{" "}
+                        Déconnexion
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
 
 export default Header
